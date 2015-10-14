abee613e57add7c33648e8bc73ec42960c1855e9
HXN User <xf03id@xf03id-srv1.cs.nsls2.local>
2015-10-14 11:42:55 -0400

    Add scan status subscription handler

---
+++ hxntools/scan_status.py
@@ -0,0 +1,45 @@
+import logging
+
+from ophyd.controls import EpicsSignal
+try:
+    from ophyd.utils import DisconnectedError
+except Exception as ex:
+    # TODO: update ophyd (didn't realize this wasn't updated and
+    # don't want to do this right before the weekend... KL 10/9)
+    DisconnectedError = Exception
+
+logger = logging.getLogger(__name__)
+
+
+class HxnScanStatus:
+    '''Broadcast if a scan is in progress via PV
+
+    Processed on every start/end document
+    '''
+    def __init__(self, running_pv):
+        self._last_start = None
+        self.running_signal = EpicsSignal(running_pv)
+        self._running = False
+
+    @property
+    def running(self):
+        return self._running
+
+    @running.setter
+    def running(self, running):
+        self._running = running
+
+        if running is None:
+            running = 0
+
+        try:
+            self.running_signal.put(running)
+        except DisconnectedError:
+            logger.error('ScanRunning PV disconnected. Is the hxntools IOC running?')
+
+    def __call__(self, name, doc):
+        '''Bluesky callback with document info'''
+        if name == 'start':
+            self._last_start = doc
+        if self._last_start and name in ('start', 'stop'):
+            self.running = (name == 'start')
